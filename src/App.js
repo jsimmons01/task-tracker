9725ef9d318ab1924a5f4d22d087661a9d006717
Jahmella Williams <jahmellasimmons@gmail.com>
2022-08-25 00:05:45 -0400

    Updates AddTask

--- src/App.js
+++ src/App.js
@@ -37,11 +37,19 @@ const App = () => {
   const toggleReminder = (id) => {
     setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
   }
-  return (
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+
+ return (
     <div className='container'>
   
      <Header />
-     <AddTask />
+     <AddTask onAdd={addTask} />
      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Task to Show' }
     </div>
     
